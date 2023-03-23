2b0f57765a23a2ae4f093d9bdd7ab1e764d99cff
Augustin Anders <augustinanders@gmail.com>
2023-03-23 14:17:08 +0100

    fix reload-function

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ import { Chance } from "chance";
 import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
+  const chance = new Chance();
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
   response.end(
     `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
   );
 });
-const chance = new Chance();
-
-const name = chance.name();
-const age = chance.age();
-const profession = chance.profession();
